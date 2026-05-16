c21754e2cff884f2a37ba4eba308de00b6c1b42e
erickroro68 <36974044+erickroro68@users.noreply.github.com>
2026-05-16 17:43:13 -0500

    Initial commit: made our first 2 master prompts
    they are custom master prompts i use for e commerce research using chatGPT agents base on using the respective prompt

---
+++ HelpFul_Prompts.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import pyperclip
+
+PROMPT_PATHS = {
+    "1": {
+        "name_of_prompt": " Coding Archetecture Teacher Helper  Prompt ",
+        "prompt_file_path": r"X:\UsefulPrompts\Coding_TEACHER_prompt_coding_atlas.txt"
+    },
+    "2": {
+        "name_of_prompt": " Selling Researcher Agent Prompt",
+        "prompt_file_path": r"X:\UsefulPrompts\E_COmmernce_AGENT_PROMPT "
+    },
+
+}   
